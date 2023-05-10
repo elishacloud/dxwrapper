1865d67914247bda8ad64262e80675228e025504
Daniel Kollmann <dankolle@mail.de>
2023-05-11 00:04:50 +0200

    Added debug window for matrices.

--- ddraw/IDirect3DDeviceX.h
+++ ddraw/IDirect3DDeviceX.h
@@ -27,6 +27,9 @@ private:
 	// SetTexture array
 	LPDIRECTDRAWSURFACE7 AttachedTexture[8] = {};
 
+	// Store debug matrix information
+	D3DMATRIX worldMatrix, viewMatrix, projectionMatrix;
+
 	// Wrapper interface functions
 	inline REFIID GetWrapperType(DWORD DirectXVersion)
 	{
@@ -74,6 +77,10 @@ public:
 		{
 			LOG_LIMIT(3, "Creating interface " << __FUNCTION__ << " (" << this << ") v" << DirectXVersion);
 		}
+
+		std::memset(&worldMatrix, 0, sizeof(D3DMATRIX));
+		std::memset(&viewMatrix, 0, sizeof(D3DMATRIX));
+		std::memset(&projectionMatrix, 0, sizeof(D3DMATRIX));
 
 		InitDevice(DirectXVersion);
 	}
