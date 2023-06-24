2c112dcea6e94ac23c6b0ab314ba7811cff7656d
Elisha Riedlinger <elisha@novicemail.com>
2023-06-24 10:06:05 -0700

    Add remarks

--- ddraw/IDirectDrawSurfaceX.cpp
+++ ddraw/IDirectDrawSurfaceX.cpp
@@ -3132,6 +3132,7 @@ LPDIRECT3DTEXTURE9 m_IDirectDrawSurfaceX::Get3DTexture()
 
 LPDIRECT3DSURFACE9 m_IDirectDrawSurfaceX::GetD3D9Surface()
 {
+	// Get d3d9 device buffer
 	/*if (IsDirect3DSurface && IsSurface3DDevice())
 	{
 		if (!pBackBuffer3D && SUCCEEDED(CheckInterface(__FUNCTION__, true, false)))
