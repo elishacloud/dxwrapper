1f9f9e19bf9eb76c893e5a1955e9634ff5106de2
Elisha Riedlinger <elisha@novicemail.com>
2023-08-10 10:41:34 -0700

    Add wrappers for cryptbase and winspool

---
+++ Wrappers/cryptbase.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define VISIT_PROCS_CRYPTBASE(visit) \
+	visit(SystemFunction001, jmpaddr) \
+	visit(SystemFunction002, jmpaddr) \
+	visit(SystemFunction003, jmpaddr) \
+	visit(SystemFunction004, jmpaddr) \
+	visit(SystemFunction005, jmpaddr) \
+	visit(SystemFunction028, jmpaddr) \
+	visit(SystemFunction029, jmpaddr) \
+	visit(SystemFunction034, jmpaddr) \
+	visit(SystemFunction036, jmpaddr) \
+	visit(SystemFunction040, jmpaddr) \
+	visit(SystemFunction041, jmpaddr)
+
+#ifdef PROC_CLASS
+PROC_CLASS(cryptbase, dll, VISIT_PROCS_CRYPTBASE, VISIT_PROCS_BLANK)
+#endif
