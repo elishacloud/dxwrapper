23e03db6f6c96ec4524b0ea6dbac38ded914b841
Elisha Riedlinger <elisha@novicemail.com>
2024-03-19 14:12:43 -0700

    Add build scripts for dx7 and dx8

---
+++ Resources/dx7-script.cmd
@@ -0,0 +1,33 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if "%~1"=="" (
+    echo Usage: %~nx0 input_file output_file
+    exit /b 1
+)
+
+set "input_file=%~1"
+set "output_file=%~2"
+
+set "search=Dd7to9"
+
+if not exist "%input_file%" (
+    echo Input file not found: %input_file%
+    exit /b 1
+)
+
+(for /f "tokens=1,* delims=]" %%a in ('find /n /v "" ^< "%input_file%"') do (
+    set "line=%%b"
+    if defined line (
+        set "line=!line: =!"
+        if "!line:~0,6!"=="%search%" (
+            echo %search%                     = 1
+        ) else if not "!line!"=="" (
+            echo %%b
+        )
+    ) else (
+        echo.
+    )
+)) > "%output_file%"
+
+echo Output file created: %output_file%
