801a67425e69ffeef41457b72ebfc66a91861167
Elisha Riedlinger <elisha@novicemail.com>
2025-04-06 21:39:01 -0700

    Disable LimitStateBlocks, ForceSingleBeginEndScene

--- Settings/Settings.cpp
+++ Settings/Settings.cpp
@@ -469,8 +469,6 @@ void Settings::SetDefaultConfigSettings()
 	Config.DdrawResolutionHack = NOT_EXIST;
 	Config.CacheClipPlane = NOT_EXIST;
 	Config.EnvironmentMapCubeFix = NOT_EXIST;
-	Config.LimitStateBlocks = NOT_EXIST;
-	Config.ForceSingleBeginEndScene = NOT_EXIST;
 	Config.WindowModeGammaShader = NOT_EXIST;
 
 	// Other values that may not exist in ini file
@@ -770,7 +768,5 @@ void CONFIG::SetConfig()
 	DdrawResolutionHack = (DdrawResolutionHack != 0);
 	CacheClipPlane = (CacheClipPlane != 0);
 	EnvironmentMapCubeFix = (EnvironmentMapCubeFix != 0);
-	LimitStateBlocks = (LimitStateBlocks != 0);
-	ForceSingleBeginEndScene = (ForceSingleBeginEndScene != 0);
 	WindowModeGammaShader = (WindowModeGammaShader != 0);
 }
