e0ac242391a2a4e4998d02567ba7a6352514fbb5
Elisha Riedlinger <elisha@novicemail.com>
2025-07-31 22:36:40 -0700

    Fix warnings from MemoryModule

---
+++ Utils/MemoryModule.cpp
@@ -0,0 +1,4 @@
+#pragma warning(push)          // Save current warning state
+#pragma warning(disable: 4996) // Disable warning C4996
+#include "External\MemoryModule\MemoryModule.c"
+#pragma warning(pop)           // Restore previous warning state
